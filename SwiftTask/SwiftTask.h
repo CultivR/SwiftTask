839e9cb31e1b54c574108b6c3e1ae086d64840fe
Jordan Kay <jordanekay@mac.com>
2018-07-30 10:46:14 -0700

    Initial Commit

---
+++ SwiftTask/SwiftTask.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTask.h
+//  SwiftTask
+//
+//  Created by Jordan Kay on 7/30/18.
+//  Copyright © 2018 Cultivr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTask.
+FOUNDATION_EXPORT double SwiftTaskVersionNumber;
+
+//! Project version string for SwiftTask.
+FOUNDATION_EXPORT const unsigned char SwiftTaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTask/PublicHeader.h>
+
+
